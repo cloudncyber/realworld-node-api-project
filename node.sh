5d3bb12c7607b1508b6163898c33cfc42c640f1c
cloudncyber <104633651+cloudncyber@users.noreply.github.com>
2024-06-22 22:10:12 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,4 +1,2 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-node -e "console.log('Running Node.js ' + process.version)"
+sudo apt install node -y
+sudo apt install npm -y
